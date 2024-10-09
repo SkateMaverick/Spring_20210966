911ddcb2a50ca918a48c263da39b4e934b92239b
SkateMaverick <avgn7410@gmail.com>
2024-10-09 14:39:27 +0900

    커밋

--- src/main/java/com/example1/demo/controller/DemoController.java
+++ src/main/java/com/example1/demo/controller/DemoController.java
@@ -1,12 +1,16 @@
 package com.example1.demo.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example1.demo.model.domain.Article;
 import com.example1.demo.model.domain.TestDB;
 import com.example1.demo.model.service.TestService;
+import com.example1.demo.model.service.blogService;
 
 
 
@@ -19,6 +23,8 @@ public String hello(Model model) {
     model.addAttribute("data", " 방갑습니다."); // model 설정
         return "hello"; // hello.html 연결
     }
+
+    
 
 @GetMapping("/hello2")
     public String hello2(Model model) {
@@ -35,6 +41,13 @@ public String hello(Model model) {
         return "about_detailed";
     }
 
+@GetMapping("/article_list")
+    public String article_list(Model model) {
+        List<Article> list = blogService.findAll(); // 게시판 리스트
+        model.addAttribute("articles", list); // 모델에 추가
+        return "article_list"; // .HTML 연결
+    }
+
 @GetMapping("/thymeleaf_test1")
     public String thymeleaf_test1(Model model) {
         model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
@@ -48,13 +61,18 @@ public String hello(Model model) {
 
 @Autowired
 TestService testService;
-
 @GetMapping("/testdb")
 public String getAllTestDBs(Model model) {
-    TestDB test = testService.findByName("홍길동");
+    TestDB test = testService.findByName("김산");
+    TestDB test1 = testService.findByName("아저씨");
+    TestDB test2 = testService.findByName("꾸러기");
         model.addAttribute("data4", test);
+        model.addAttribute("data5", test1);
+        model.addAttribute("data6", test2);
     System.out.println("데이터 출력 디버그 : " + test);
     return "testdb";
 }
+
+
     
 }
