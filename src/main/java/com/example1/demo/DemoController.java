616821141a4f8c7f826bb358b5253a0c2e17ec32
SkateMaverick <avgn7410@gmail.com>
2024-10-09 13:23:45 +0900

    동기화ㅣ

--- src/main/java/com/example1/demo/DemoController.java
+++ src/main/java/com/example1/demo/DemoController.java
@@ -2,6 +2,8 @@ package com.example1.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -9,9 +11,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
 @GetMapping("/hello") // 전송 방식 GET
-
 public String hello(Model model) {
     model.addAttribute("data", " 방갑습니다."); // model 설정
         return "hello"; // hello.html 연결
     }
+
+@GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("name", "김산님,");
+        model.addAttribute("hi", "반갑습니다,");
+        model.addAttribute("today", "오늘,");
+        model.addAttribute("weather", "날씨는,");
+        model.addAttribute("good", "매우 좋습니다.");
+        return "hello2";
+    }
+
+@GetMapping("/about_detailed")
+    public String about(){
+        return "about_detailed";
+    }
+
+    
+
+    
 }
