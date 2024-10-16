7f236dc148889866bb23cc08aaa2da485149c92e
SkateMaverick <avgn7410@gmail.com>
2024-10-16 13:03:32 +0900

    .

--- src/main/java/com/example1/demo/model/service/AddArticleRequest.java
+++ src/main/java/com/example1/demo/model/service/AddArticleRequest.java
@@ -4,13 +4,15 @@ import com.example1.demo.model.domain.Article;
 @NoArgsConstructor // 기본 생성자 추가
 @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
 @Data // getter, setter, toString, equals 등 자동 생성
+
+
 public class AddArticleRequest {
-private String title;
-private String content;
-public Article toEntity(){ // Article 객체 생성
-return Article.builder()
-.title(title)
-.content(content)
-.build();
-}
+    private String title;
+    private String content;
+    public Article toEntity(){ // Article 객체 생성
+        return Article.builder()
+            .title(title)
+            .content(content)
+            .build();
+    }
 }
